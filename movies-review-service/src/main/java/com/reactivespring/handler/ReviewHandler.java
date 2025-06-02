de46cd9abef89539fd5cb06b92addf032b1111af
VHN <vunam12994@gmail.com>
2025-06-02 21:47:41 +0700

    init

---
+++ movies-review-service/src/main/java/com/reactivespring/handler/ReviewHandler.java
@@ -0,0 +1,64 @@
+package com.reactivespring.handler;
+
+import com.reactivespring.domain.Review;
+import com.reactivespring.repository.ReviewReactiveRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.server.ServerRequest;
+import org.springframework.web.reactive.function.server.ServerResponse;
+import reactor.core.publisher.Mono;
+
+@Component
+public class ReviewHandler {
+
+    private ReviewReactiveRepository reviewReactiveRepository;
+
+    public ReviewHandler(ReviewReactiveRepository reviewReactiveRepository) {
+        this.reviewReactiveRepository = reviewReactiveRepository;
+    }
+
+    public Mono<ServerResponse> addReview(ServerRequest request) {
+
+        return request.bodyToMono(Review.class)
+                .flatMap(reviewReactiveRepository::save)
+                .flatMap(ServerResponse.status(HttpStatus.CREATED)::bodyValue);
+
+    }
+
+    public Mono<ServerResponse> getReviews(ServerRequest request) {
+
+        var reviewsFlux = reviewReactiveRepository.findAll();
+        return ServerResponse.ok().body(reviewsFlux, Review.class);
+
+    }
+
+    public Mono<ServerResponse> updateReview(ServerRequest request) {
+
+        var reviewId = request.pathVariable("id");
+
+        var existingReview = reviewReactiveRepository.findById(reviewId);
+
+        return existingReview
+                .flatMap(review -> request.bodyToMono(Review.class)
+                        .map(reqReview -> {
+                            review.setComment(reqReview.getComment());
+                            review.setRating(reqReview.getRating());
+                            return review;
+                        })
+                        .flatMap(reviewReactiveRepository::save)
+                        .flatMap(savedReview -> ServerResponse.ok().bodyValue(savedReview))
+                );
+    }
+
+    public Mono<ServerResponse> deleteReview(ServerRequest request) {
+
+        var reviewId = request.pathVariable("id");
+
+        var existingReview = reviewReactiveRepository.findById(reviewId);
+
+        return existingReview
+                .flatMap(review -> reviewReactiveRepository.deleteById(reviewId)
+                        .flatMap(reviewDeleted -> ServerResponse.noContent().build()));
+
+    }
+}
